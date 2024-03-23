6b10e77671939f238fb6d1435e8847fb4b6c027e
Francislie <mfrancislie@gmail.com>
2024-03-23 00:20:41 -0700

    Action in livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -8,9 +8,17 @@ class CreatePoll extends Component
 {
 
     public $title;
-    
+    public $options = ['First'];
+
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+
 }
